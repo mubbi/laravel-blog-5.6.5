578ffe1ce23e9a62f9d891fb98780f0e498ad5a6
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-02-25 00:24:19 +0500

    Updated Views

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# laravel-blog-5.6.5
+# Laravel Blog - v5.6.5
+This is a sample blog based on Laravel 5.6 framework and Bootstrap 4.
+# Features!
+  - Blogs/Articles
+  - Categories
+  - Comments
+  - Featured Image
+  - Admin Area
+  - RSS Feed
+
+
+### Tech
+This blog project uses open source projects to work properly:
+* [Twitter Bootstrap] - great UI boilerplate for modern web apps
+* [Laravel] - Framework for Web Artisans
+* [jQuery] - duh
+
+And of course blog project itself is open source with a public repository on GitHub.
+
+License
+----
+
+GNU GPLv3
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -2,6 +2,13 @@
     <div class="card-header">Sidebar</div>
 
     <div class="card-body">
-        More codes coming soon!
+        <h3>Categories</h3>
+        <ul class="list-group">
+          <li class="list-group-item"><a href="#">Category 1</a></li>
+          <li class="list-group-item"><a href="#">Category 2</a></li>
+          <li class="list-group-item"><a href="#">Category 3</a></li>
+          <li class="list-group-item"><a href="#">Category 4</a></li>
+          <li class="list-group-item"><a href="#">Category 5</a></li>
+        </ul>
     </div>
 </div>
