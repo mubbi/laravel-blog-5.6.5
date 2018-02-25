e9fe919c9f1272d74f86288124763dace9dca95b
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-02-25 13:41:01 +0500

    Dynamic Sidebar
    Updated Appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Share sidebar Categories with all views
+        $categories = Category::select('name', 'slug')->get();
+        View::share('sidebar_categories', $categories);
     }
 
     /**
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,14 +1,11 @@
 <div class="card">
     <div class="card-header">Sidebar</div>
-
     <div class="card-body">
         <h3>Categories</h3>
         <ul class="list-group">
-          <li class="list-group-item"><a href="#">Category 1</a></li>
-          <li class="list-group-item"><a href="#">Category 2</a></li>
-          <li class="list-group-item"><a href="#">Category 3</a></li>
-          <li class="list-group-item"><a href="#">Category 4</a></li>
-          <li class="list-group-item"><a href="#">Category 5</a></li>
+          @foreach($sidebar_categories as $category)
+          <li class="list-group-item"><a href="{{ url('category/'.$category->slug) }}">{{ $category->name }}</a></li>
+          @endforeach
         </ul>
     </div>
 </div>
