992176737d65ecbde8f91b2eb9494b67d1179736
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-02-28 17:39:25 +0500

    added settings support

---
+++ database/migrations/2018_02_28_122012_create_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('setting_name', 150)->unique()->nullable(false);
+            $table->text('setting_value')->nullable(false);
+            $table->string('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
