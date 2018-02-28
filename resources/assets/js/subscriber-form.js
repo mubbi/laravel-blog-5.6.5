99557f2519402fc0797355389813566342279eef
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-02-28 19:58:46 +0500

    update

--- resources/assets/js/subscriber-form.js
+++ resources/assets/js/subscriber-form.js
@@ -1,27 +1,28 @@
 $("#subscribe_form").submit(function(e) {
 
-    if ( $("#subscribe_form #email_id").val() == '' ) {
+    if ( $("#subscribe_form #email").val() == '' ) {
         alert('Email Required');
-        return false;
     }
-    // Submit Ajax Request
-    $.ajax({
-        url: '$("#subscribe_form").attr("action")',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            email: $("#subscribe_form #email").val()
-        },
-    })
-    .done(function() {
-        console.log("success");
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() {
-        console.log("complete");
-    });
+    else {
+        // Submit Ajax Request
+        $.ajax({
+            url: '$("#subscribe_form").attr("action")',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                email: $("#subscribe_form #email").val()
+            },
+        })
+        .done(function() {
+            console.log("success");
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+    }
 
     // Stop Form
     e.preventDefault();
