777078052681a0ab0db66856c32d58d66fe252c4
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-01 22:34:19 +0500

    bug fixed

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Blog::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(5),
-        'image' => $faker->image('public/storage/images', 700, 450, null, false),
+        'image' => $faker->image('images', 700, 450, null, false),
         'excerpt' => $faker->text,
         'description' => $faker->paragraphs,
         'views' => 0
