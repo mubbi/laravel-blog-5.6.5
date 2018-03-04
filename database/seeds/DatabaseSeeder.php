d8589c8ab406ce376f72457d215bbcc2643d2a1a
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-04 17:26:50 +0500

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         // Model::unguard();
         $this->call(
-            UsersTableSeeder::class
-            // SettingsTableSeeder::class
+            UsersTableSeeder::class,
+            SettingsTableSeeder::class
             // CategoriesTableSeeder::class,
             // BlogsTableSeeder::class,
         );
