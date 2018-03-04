f2c14596b0ffaa36f50d003d35cc97de7bce1e3f
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-04 21:11:44 +0500

    roles seeder
    more roles will be added here

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->truncate();
+
+        Role::create([
+            'role' => 'add_blog',
+            'description' => 'Permission to create a blog'
+        ]);
+    }
+}
