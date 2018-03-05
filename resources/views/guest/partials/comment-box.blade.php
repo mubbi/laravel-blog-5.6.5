91e8fc1a22172dc73a344c5fb83e229d9eb308f3
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-06 02:46:34 +0500

    added comment disable feature

--- resources/views/guest/partials/comment-box.blade.php
+++ resources/views/guest/partials/comment-box.blade.php
@@ -7,26 +7,30 @@
         <div class="row">
             <div class="col-md-12">
 
-                <form action="{{ url('post/comment') }}" method="post">
-                    {{ csrf_field() }}
-                    <div class="form-row">
-                        <div class="col-md-6">
-                            <label for="name">Name <span class="required">*</span></label>
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Your Name" required>
+                @if($blog->allow_comments == 1)
+                    <form action="{{ url('post/comment') }}" method="post">
+                        {{ csrf_field() }}
+                        <div class="form-row">
+                            <div class="col-md-6">
+                                <label for="name">Name <span class="required">*</span></label>
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Your Name" required>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="email">Email <span class="required">*</span></label>
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Your Email ID" required>
+                            </div>
+                            <div class="col-md-12 mt-3">
+                                <label for="name">Your Comment <span class="required">*</span></label>
+                                <textarea name="body" class="form-control" rows="4" placeholder="Write something nice..." required>{{ old('body') }}</textarea>
+                            </div>
+                            <div class="col-md-12 mt-3">
+                                <button type="submit" class="btn btn-sm btn-primary"><i class="fas fa-comment"></i> Submit</button>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <label for="email">Email <span class="required">*</span></label>
-                            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Your Email ID" required>
-                        </div>
-                        <div class="col-md-12 mt-3">
-                            <label for="name">Your Comment <span class="required">*</span></label>
-                            <textarea name="body" class="form-control" rows="4" placeholder="Write something nice..." required>{{ old('body') }}</textarea>
-                        </div>
-                        <div class="col-md-12 mt-3">
-                            <button type="submit" class="btn btn-sm btn-primary"><i class="fas fa-comment"></i> Submit</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                @else
+                <h4>Comments are disabled!</h4>
+                @endif
 
             </div>
         </div>
