894ec3af4e9b91dd5d4e27c91e9a875c10cb3e84
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-07 21:48:13 +0500

    integerated Laravel Filemanager with TinyMCE
    Added Image upload support with laravel file manager for TinyMCE js plugin

--- resources/views/admin/blogs/create.blade.php
+++ resources/views/admin/blogs/create.blade.php
@@ -78,7 +78,9 @@
 <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
 <script>
 // Integrate TinyMCE Editor
-tinymce.init({
+// Make Config Settings
+var editor_config = {
+    path_absolute : base_url,
     selector:'#description',
     height: 500,
     theme: 'modern',
@@ -89,7 +91,30 @@ tinymce.init({
     ],
     toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media fullscreen",
     image_advtab: true,
-});
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + '/tinymce/filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+// Init TinyMCE
+tinymce.init(editor_config);
 
 $(document).ready(function() {
     $('#categories').select2({
