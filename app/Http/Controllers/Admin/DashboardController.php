0eddedf30e68274d8f61ec9ce2bdae84b9a88eac
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-07 20:39:43 +0500

    Auth Middleware combined with Roles Middleware
    Just added Auth:check()  to the CheckRole middleware so that we dont have to worry about the user is logged in or not and also now we can make our controllers/route free of auth middleware

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,7 +14,6 @@ class DashboardController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
     }
 
     /**
--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,13 +16,21 @@ class CheckRole
      */
     public function handle($request, Closure $next, $role)
     {
+        // Added "Auth Middleware" functionality to detect Auth status
+        // So that we dont have to also add the "auth middleware" to our routes/controllers
         if (!Auth::check()) {
             return redirect('login');
         }
+
+        // Get user
         $user = Auth::user();
+
+        // Check its role
         if ($user->hasRole($role)) {
             return $next($request);
         }
+
+        // If user dont have the role let him know
         return redirect('unauthorized');
     }
 }
