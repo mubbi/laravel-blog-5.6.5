27d938e26861ac8008dcefd661e0f5104753ea98
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-08 21:08:15 +0500

    added sub title in navbar
    added sub title from settings in nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ app('global_settings')[0]['setting_value'] }}
+                    {{ app('global_settings')[0]['setting_value'] }} - <small>{{ app('global_settings')[1]['setting_value'] }}</small>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
