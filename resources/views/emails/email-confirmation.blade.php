3afb2d4b6b168c51042996bc8c902257ed2674eb
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-09 03:09:14 +0500

    update

--- resources/views/emails/email-confirmation.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<div class="card">
-    <div class="card-header">Registration Confirmed</div>
-    <div class="card-body">
-        Your Email is successfully verified. Click here to <a href="{{ url('/login') }}">login</a>
-    </div>
-</div>
-@endsection
--- resources/views/emails/verification-msg.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<div class="card">
-    <div class="card-header">Registration</div>
-    <div class="card-body">
-        You have successfully registered. An email is sent to you for verification.
-    </div>
-</div>
-@endsection
