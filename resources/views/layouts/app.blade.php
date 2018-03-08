e7d41a45c06b1adb929fee18a282c4c1a32f76aa
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-08 18:40:58 +0500

    added manage blogs link
    to improve UX and quick jump to manage blogs section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,7 @@
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a>
+                                    <a class="dropdown-item" href="{{ route('blogs.index') }}">Manage blogs</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
