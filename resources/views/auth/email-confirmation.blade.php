6bfaa3ea93b29b8a3d0ece42ae926e36349aedcf
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2018-03-09 01:34:11 +0500

    updated view to card
    removed old panel codes and added card codes

--- resources/views/auth/email-confirmation.blade.php
+++ resources/views/auth/email-confirmation.blade.php
@@ -1,15 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Registration Confirmed</div>
-                <div class="panel-body">
-                    Your Email is successfully verified. Click here to <a href="{{ url('/login') }}">login</a>
-                </div>
-            </div>
-        </div>
+<div class="card">
+    <div class="card-header">Registration Confirmed</div>
+    <div class="card-body">
+        Your Email is successfully verified. Click here to <a href="{{ url('/login') }}">login</a>
     </div>
 </div>
 @endsection
--- resources/views/auth/verification-msg.blade.php
+++ resources/views/auth/verification-msg.blade.php
@@ -1,15 +1,9 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Registration</div>
-                <div class="panel-body">
-                    You have successfully registered. An email is sent to you for verification.
-                </div>
-            </div>
-        </div>
+<div class="card">
+    <div class="card-header">Registration</div>
+    <div class="card-body">
+        You have successfully registered. An email is sent to you for verification.
     </div>
 </div>
 @endsection
